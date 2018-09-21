de16d8600c1b9827b3f2011de33a91e3418e057c
Remon Karim <remon@life360.com>
2018-09-21 16:10:45 -0700

    Testing FROM a android image by circleci. With sudo su.

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-echo "FROM buildpack-deps:$(awk -F'_' '{print tolower($2)}' <<< $LINUX_VERSION)"
+# remon.add
+echo "FROM circleci/android:api-26-alpha"
 
-echo "RUN apt-get update"
+### echo "FROM buildpack-deps:$(awk -F'_' '{print tolower($2)}' <<< $LINUX_VERSION)"
+
+echo "RUN sudo apt-get update"
 
 if [ ! -e $RUBY_VERSION_NUM ] ; then
-    echo "RUN apt-get install -y libssl-dev && wget http://ftp.ruby-lang.org/pub/ruby/$(awk -F'.' '{ print $1"."$2 }' <<< $RUBY_VERSION_NUM)/ruby-$RUBY_VERSION_NUM.tar.gz && \
+    echo "RUN sudo apt-get install -y libssl-dev && wget http://ftp.ruby-lang.org/pub/ruby/$(awk -F'.' '{ print $1"."$2 }' <<< $RUBY_VERSION_NUM)/ruby-$RUBY_VERSION_NUM.tar.gz && \
     tar -xzvf ruby-$RUBY_VERSION_NUM.tar.gz && \
     cd ruby-$RUBY_VERSION_NUM/ && \
     ./configure && \
@@ -43,35 +46,35 @@ if [ $JAVA = "true" ] ; then
 cat << EOF
 RUN if [ \$(grep 'VERSION_ID="8"' /etc/os-release) ] ; then \\
     echo "deb http://ftp.debian.org/debian jessie-backports main" >> /etc/apt/sources.list && \\
-    apt-get update && apt-get -y install -t jessie-backports openjdk-8-jdk ca-certificates-java \\
+    sudo apt-get update && sudo apt-get -y install -t jessie-backports openjdk-8-jdk ca-certificates-java \\
 ; elif [ \$(grep 'VERSION_ID="9"' /etc/os-release) ] ; then \\
-		apt-get update && apt-get -y -q --no-install-recommends install -t stable openjdk-8-jdk ca-certificates-java \\
+		sudo apt-get update && sudo apt-get -y -q --no-install-recommends install -t stable openjdk-8-jdk ca-certificates-java \\
 ; elif [ \$(grep 'VERSION_ID="14.04"' /etc/os-release) ] ; then \\
-		apt-get update && \\
-    apt-get --force-yes -y install software-properties-common python-software-properties && \\
+		sudo apt-get update && \\
+    sudo apt-get --force-yes -y install software-properties-common python-software-properties && \\
     echo | add-apt-repository -y ppa:webupd8team/java && \\
-    apt-get update && \\
+    sudo apt-get update && \\
     echo debconf shared/accepted-oracle-license-v1-1 select true | debconf-set-selections && \\
     echo debconf shared/accepted-oracle-license-v1-1 seen true | debconf-set-selections && \\
-    apt-get -y install oracle-java8-installer \\
+    sudo apt-get -y install oracle-java8-installer \\
 ; elif [ \$(grep 'VERSION_ID="16.04"' /etc/os-release) ] ; then \\
-    apt-get update && \\
-    apt-get --force-yes -y install software-properties-common python-software-properties && \\
+    sudo apt-get update && \\
+    sudo apt-get --force-yes -y install software-properties-common python-software-properties && \\
     echo | add-apt-repository -y ppa:webupd8team/java && \\
-    apt-get update && \\
+    sudo apt-get update && \\
     echo debconf shared/accepted-oracle-license-v1-1 select true | debconf-set-selections && \\
     echo debconf shared/accepted-oracle-license-v1-1 seen true | debconf-set-selections && \\
-    apt-get -y install oracle-java8-installer \\
+    sudo apt-get -y install oracle-java8-installer \\
 ; fi
 EOF
 fi
 
 if [ $MYSQL_CLIENT = "true" ] ; then
-    echo "RUN apt-get -y install mysql-client"
+    echo "RUN sudo apt-get -y install mysql-client"
 fi
 
 if [ $POSTGRES_CLIENT = "true" ] ; then
-    echo "RUN apt-get -y install postgresql-client"
+    echo "RUN sudo apt-get -y install postgresql-client"
 fi
 
 if [ $DOCKERIZE = "true" ] ; then
@@ -96,15 +99,15 @@ echo "RUN perl -MCPAN -e 'install TAP::Parser'"
 echo "RUN perl -MCPAN -e 'install XML::Generator'"
 
 # install lsb-release, etc., for testing linux distro
-echo "RUN apt-get update && apt-get -y install lsb-release unzip"
+echo "RUN sudo apt-get update && sudo apt-get -y install lsb-release unzip"
 
 if [ $BROWSERS = "true" ] ; then
 cat << EOF
 RUN if [ \$(grep 'VERSION_ID="8"' /etc/os-release) ] ; then \\
     echo "deb http://ftp.debian.org/debian jessie-backports main" >> /etc/apt/sources.list && \\
-    apt-get update && apt-get -y install -t jessie-backports xvfb phantomjs \\
+    sudo apt-get update && sudo apt-get -y install -t jessie-backports xvfb phantomjs \\
 ; else \\
-		apt-get update && apt-get -y install xvfb phantomjs \\
+		sudo apt-get update && sudo apt-get -y install xvfb phantomjs \\
 ; fi
 EOF
 echo "ENV DISPLAY :99"
@@ -112,19 +115,19 @@ echo "ENV DISPLAY :99"
 echo "# install firefox
 RUN curl --silent --show-error --location --fail --retry 3 --output /tmp/firefox.deb https://s3.amazonaws.com/circle-downloads/firefox-mozilla-build_47.0.1-0ubuntu1_amd64.deb \
   && echo 'ef016febe5ec4eaf7d455a34579834bcde7703cb0818c80044f4d148df8473bb  /tmp/firefox.deb' | sha256sum -c \
-  && dpkg -i /tmp/firefox.deb || apt-get -f install  \
-  && apt-get install -y libgtk3.0-cil-dev libasound2 libasound2 libdbus-glib-1-2 libdbus-1-3 \
+  && dpkg -i /tmp/firefox.deb || sudo apt-get -f install  \
+  && sudo apt-get install -y libgtk3.0-cil-dev libasound2 libasound2 libdbus-glib-1-2 libdbus-1-3 \
   && rm -rf /tmp/firefox.deb"
 
 echo "# install chrome
 RUN curl --silent --show-error --location --fail --retry 3 --output /tmp/google-chrome-stable_current_amd64.deb https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb \
-  && (dpkg -i /tmp/google-chrome-stable_current_amd64.deb || apt-get -fy install)  \
+  && (dpkg -i /tmp/google-chrome-stable_current_amd64.deb || sudo apt-get -fy install)  \
   && rm -rf /tmp/google-chrome-stable_current_amd64.deb \
   && sed -i 's|HERE/chrome\"|HERE/chrome\" --disable-setuid-sandbox --no-sandbox|g' \
        \"/opt/google/chrome/google-chrome\""
 
 echo "# install chromedriver
-RUN apt-get -y install libgconf-2-4 \
+RUN sudo apt-get -y install libgconf-2-4 \
   && curl --silent --show-error --location --fail --retry 3 --output /tmp/chromedriver_linux64.zip \"http://chromedriver.storage.googleapis.com/2.33/chromedriver_linux64.zip\" \
   && cd /tmp \
   && unzip chromedriver_linux64.zip \
